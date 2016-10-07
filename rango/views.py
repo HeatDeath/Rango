5316f23940db3f22ba2a3b9527ceccad7b949a0a
shahbaz <shahbaz.h96@gmail.com>
2016-10-08 01:16:16 +0530

    Initial Commit for Django Project

---
+++ rango/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpRequest
+
+
+def index(request):
+    return HttpResponse("Rango says Hey there Partner!")
