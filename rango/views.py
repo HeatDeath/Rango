00e18e1b42427fb57ad416ab9e1f0a0214a04820
shahbaz <shahbaz.h96@gmail.com>
2016-10-11 16:08:12 +0530

    Added Form to the html page for Adding New Category And Pages

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from rango.models import Category, Page
-from django.http import HttpResponse
-from django.http import HttpRequest
+from rango.forms import CategoryForm, PageForm
+from django.db import IntegrityError
 
 
 def index(request):
@@ -36,3 +36,49 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+
+    try:
+        if request.method == 'POST':
+            form = CategoryForm(request.POST)
+
+            if form.is_valid():
+                form.save(commit=True)
+
+                return index(request)
+
+            else:
+                print form.errors
+
+        return render(request, 'rango/add_category.html', {'form': form})
+
+    except IntegrityError as e:
+        return render(request, "rango/add_category.html", {'form': form, "message": e.message})
+
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+    form = PageForm()
+    try:
+        if request.method == 'POST':
+            form = PageForm(request.POST)
+            if form.is_valid():
+                if category:
+                    page = form.save(commit=False)
+                    page.category = category
+                    page.views = 0
+                    page.save()
+                    return show_category(request, category_name_slug)
+                else:
+                    print(form.errors)
+        context_dict = {'form': form, 'category': category}
+        return render(request, 'rango/add_page.html', context_dict)
+
+    except IntegrityError as e:
+        return render(request, 'rango/add_page.html', {'form': form, 'category': category, 'message': e.message})
